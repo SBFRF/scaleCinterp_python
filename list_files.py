ff18941cb9ab848ce548d5ae181175f722b64bb3
Spicer Bak <Spicer.Bak@usace.army.mil>
2017-03-15 11:53:31 -0400

    replaced looping with glob functionality

--- list_files.py
+++ list_files.py
@@ -4,13 +4,14 @@ Created on Thu Oct 16 14:28:56 2014
 
 @author: jwlong
 """
-import os
+import glob
 def list_files(datapath, datatype):
     # returns a list of names (with extension, without full path) of all files 
     # in folder path
-    filelist = []
-    listing = os.listdir(datapath)
-    for files in listing:
-        if files.endswith(datatype):
-           filelist.append(files)
-    return filelist 
+    # filelist = []
+    # listing = os.listdir(datapath)
+    # for files in listing:
+    #     if files.endswith(datatype):
+    #        filelist.append(files)
+    # return filelist
+    return glob.glob(datapath+'*'+datatype)  # i think this should do the same thing as above
