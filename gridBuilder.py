04c36cb836aab1d5c1cd624c95571c848d26dc5a
Spicer Bak <Spicer.Bak@usace.army.mil>
2017-06-02 14:46:43 -0400

    updated epsg code for FRF (default) but exposes to those that want to use it, minor comments, moved imports

--- gridBuilder.py
+++ gridBuilder.py
@@ -6,26 +6,28 @@ Created on Thu Oct 16 11:25:54 2014
 """
 import numpy as np  
 import pyproj
-    
-def gridBuilder(x0, x1, y0, y1, dx, dy, grid_coord_check, grid_filename):
+import scipy.io as sio
 
-    if (grid_filename == ' '):
-        if (grid_coord_check == 'LL'):
+
+def gridBuilder(x0, x1, y0, y1, dx, dy, grid_coord_check, grid_filename, EPSG=26918):
+    "uses default to EPSG code of US EST coast (FRF location)"
+    if (grid_filename.strip() == ''):
+        # build grid in UTM using dx dy, and 2 corners of grid(x0, y0)
+        if (grid_coord_check.strip() == 'LL'):
             # must convert to UTM (meters)
-            UTM16N=pyproj.Proj("+init=EPSG:32616") # UTM coords, zone 16N, WGS84 datum
+            UTM16N=pyproj.Proj("+init=EPSG:%d" %EPSG) #:32616") # UTM coords, zone 16N, WGS84 datum
             x0, y0 = UTM16N(x0, y0)
             x1, y1 = UTM16N(x1, y1)
        
-        x0 = np.round(x0, decimals=0)
+        x0 = np.round(x0, decimals=0)  # why are these rounded ??
         x1 = np.round(x1, decimals=0)  
         y0 = np.round(y0, decimals=0)  
         y1 = np.round(y1, decimals=0)  
-        numGridPointsX = (x1 - x0)/dx
+        numGridPointsX = (x1 - x0)/dx  # this assumes finite difference grid (points are vertex located)
         numGridPointsY = (y1 - y0)/dy
-        x_grid, y_grid = np.meshgrid(np.linspace(x0, x1, numGridPointsX),np.linspace(y0, y1, numGridPointsY))
+        x_grid, y_grid = np.meshgrid(np.linspace(x0, x1, numGridPointsX), np.linspace(y0, y1, numGridPointsY))
         pass
     else:
-        import scipy.io as sio
         try:
             gridFile = sio.loadmat(grid_filename) # Currently only works with MAT file
             x_grid = gridFile['xgrid'] 
