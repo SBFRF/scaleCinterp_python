68d15fdb5b2bb381ba4517cefa5d39fba8c82996
Spicer Bak <Spicer.Bak@usace.army.mil>
2017-07-06 16:49:28 -0400

    migrated the grid builder function over to the data builder.py file

--- dataBuilder.py
+++ dataBuilder.py
@@ -101,11 +101,15 @@ def readInDataSet(filename):
   
 def dataBuilder(filelist, data_coord_check, EPSG=26918):
     """
-    this function reads data sets and converts to
+    this function reads the measured data sets and converts to UTM (assumed Longitude, latitude)
+
     This function assumes UTM zone 18N at the FRF in NAD83
-    :param filelist:
+
+    :param filelist: list of files that has single time step of bathymetry measurements
+
     :param data_coord_check:
-    :param EPSG:
+
+    :param EPSG:  EPSG code used for tranlating Latitude/longitude to UTM ( assumed FRF) UTM zone 18 N
     :return:
     """
     tempX, tempY, tempZ = [], [], [] 
@@ -119,7 +123,7 @@ def dataBuilder(filelist, data_coord_check, EPSG=26918):
     z = tempZ[:,np.newaxis]
     
     if (data_coord_check == 'LL'):
-        UTM16N=pyproj.Proj("+init=EPSG:%s" % EPSG ) # 32618") # UTM coords, zone 18N, WGS84 datum  #
+        UTM16N=pyproj.Proj("+init=EPSG:%s" % EPSG )
         [xutm,yutm] = UTM16N(tempX, tempY) # convert to UTM coord
         x = np.array([xutm, yutm, np.zeros(xutm.size)]).T
 
@@ -147,3 +151,58 @@ def loadNOAAdem(filename, x0, x1, y0, y1):
     [Xprior, Yprior] = UTM16N(Xprior,Yprior)
     
     return Xprior, Yprior, Zprior
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 16 11:25:54 2014
+
+@author: jwlong
+
+# this was brought in from another file
+"""
+def gridBuilder(x0, x1, y0, y1, dx, dy, grid_coord_check, grid_filename, EPSG=26918):
+    """
+        uses default to EPSG code of US EST coast (FRF location),
+    This function could also use key word generation
+
+    Builds the grid nodes, using verticies location
+
+    :param x0: origin
+    :param x1:
+    :param y0:
+    :param y1:
+    :param dx:
+    :param dy:
+    :param grid_coord_check:
+    :param grid_filename:
+    :param EPSG:
+    :return:
+    """
+    if (grid_filename.strip() == ''):  # if there's no grid filename
+        # build grid in UTM using dx dy, and 2 corners of grid(x0, y0)
+        if (grid_coord_check.strip() == 'LL'):
+            # must convert to UTM (meters)
+            UTM16N = pyproj.Proj("+init=EPSG:%d" % EPSG)
+            x0, y0 = UTM16N(x0, y0)
+            x1, y1 = UTM16N(x1, y1)
+
+        x0 = np.round(x0, decimals=0)  # why are these rounded ?  this moves the origin of the grid?
+        x1 = np.round(x1, decimals=0)
+        y0 = np.round(y0, decimals=0)
+        y1 = np.round(y1, decimals=0)
+
+        numGridPointsX = np.abs((x1 - x0) / dx)  # this assumes finite difference grid (points are vertex located)
+        numGridPointsY = np.abs((y1 - y0) / dy)
+        x_grid, y_grid = np.meshgrid(np.linspace(x0, x1, numGridPointsX), np.linspace(y0, y1, numGridPointsY))
+        pass
+    else:
+        try:
+            gridFile = sio.loadmat(grid_filename)  # Currently only works with MAT file
+            print "here's where to get the NetCDF grid file locations"
+            x_grid = gridFile['xgrid']
+            y_grid = gridFile['ygrid']
+        except IOError:
+            print 'The file,', grid_filename, ', does not exist in the path. Please try again.'
+
+    return x_grid, y_grid
