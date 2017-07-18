5e58e38b8666446c52165d4f29e02d370a69f762
Spicer Bak <Spicer.Bak@usace.army.mil>
2017-07-18 16:43:22 -0400

    refactored DEM_generator to act more as a function

--- DEM_generator.py
+++ DEM_generator.py
@@ -80,7 +80,7 @@ def DEM_generator(dict):
                                      #      ['hanning', 'linloess', 'quadloess', 'boxcar', si']
     nmseitol = dict['nmseitol']  # Normalized error tolerance the user will tolerate in the final grid
                                                       #      (0 - (no error) to 1 (no removal of bad points))
-
+    filelist = dict['filelist']
     #### data checks ###########3
     filters = ['hanning', 'linloess', 'quadloess', 'boxcar', 'si']
     assert filtername in filters, 'Check filter name, not appropriate for current DEM generator function'
@@ -91,21 +91,22 @@ def DEM_generator(dict):
     x, z = dataBuilder(filelist, data_coord_check)  # function loads x, y, z data and concatenates in long array
     s = np.ones((np.size(x[:,1]),1))
     print 'loading time is %s seconds' % (DT.datetime.now() - t)
-
+    assert x.shape[0] > 1, 'Data Did not Load!'
     ####################################################################
     # Call grid builder to make a grid based on x,y min and max values #
     ####################################################################
 
     x_grid, y_grid = gridBuilder(x0, x1, y0, y1, lambdaX, lambdaY, grid_coord_check, grid_filename)
-    t_grid = np.zeros_like((x_grid))  # dummy values, for time?
+    t_grid = np.zeros_like((x_grid))  # Interpolate in time -- Not Developed Yet, but place holder there
     xi = np.array([x_grid.flatten(), y_grid.flatten(), t_grid.flatten()]).T  # grid locations, flatten make row-major style
+    # now make smoothing array same shape as  xi
     xsm = msmoothx*np.ones_like(x_grid)
     ysm = msmoothy*np.ones_like(y_grid)
     tsm = msmootht*np.ones_like(t_grid)
     lx = np.array([xsm.flatten(), ysm.flatten(), tsm.flatten()]).T  # smoothing array , flatten takes row-major style
 
     N, M = np.shape(x_grid)
-    x_out = x_grid[0,:].copy()  # utm output for x and y
+    x_out = x_grid[0,:].copy()  # grid coordinate output
     y_out = y_grid[:,0].copy()
 
     del x_grid, y_grid, t_grid
@@ -113,9 +114,22 @@ def DEM_generator(dict):
     # subsample the data   ##############################################
     #####################################################################
     DXsmooth = np.array([msmoothx, msmoothy, msmootht])/4
-    DXsmooth[2] = 1
+    DXsmooth[2] = 1  # this hard codes a time smoothing of 1 (unit?)
     t = DT.datetime.now()
     Xi, zprime, si = subsampleData(x, z, s, DXsmooth)
+    # a plot to compare original data to subsampled data 
+    from matplotlib import pyplot as plt
+    plt.figure()
+    plt.suptitle('%s' %filelist[0])
+    plt.subplot(211)
+    plt.plot(x[:,0], x[:,1], '.', label='Raw')
+    plt.plot(Xi[:,0], Xi[:,1], '.', label='SubSampled')
+    plt.legend()
+    plt.subplot(212)
+    plt.plot(np.sqrt(x[:, 0]**2 + x[:, 1]**2), z, '.', label='raw')
+    plt.plot( np.sqrt(Xi[:,0]**2 + Xi[:,1]**2), zprime, '.', label='subsampled')
+    plt.legend()
+
     # What's returned here
     print 'subsampling time is %s seconds' % (DT.datetime.now() - t)
 
@@ -141,7 +155,9 @@ def DEM_generator(dict):
     out = {'Zi': zi,
            'MSEi': msei,
            'NMSEi': nmsei,
-           'MSRi': msri}
+           'MSRi': msri,
+           'x_out': x_out,
+           'y_out': y_out}
     return out
 
 
@@ -151,96 +167,72 @@ def DEM_generator(dict):
 
 
 # ########## scratch
-from sblib import geoprocess as gp
-
-# Call dataBuilder to construct data in necessary format for interpolation
-# filelist = list_files(datapath, datatype)  # creates a list of files to be interpolated
-filelist = ['FRF_20170518_1134_FRF_NAVD88_LARC_GPS_UTC_v20170525.nc',]  # files with NEW data that are in background grid
-
-x0, y0 = 1600, 1600  # north east corner of grid
-x1, y1 = 0, -200     # south west corner of grid
-dict = {'x0': gp.FRFcoord(x0, y0)['Lon'],  # -75.47218285,
-        'y0': gp.FRFcoord(x0, y0)['Lat'],  #  36.17560399,
-        'x1': gp.FRFcoord(x1, y1)['Lon'],  # -75.75004989,
-        'y1': gp.FRFcoord(x1, y1)['Lat'],  #  36.19666112,
-        'lambdaX': 10,  # grid spacing in x  -  Here is where CMS would hand array of variable grid spacing
-        'lambdaY': 10,  # grid spacing in y
-        'msmoothx': 100,  # smoothing length scale
-        'msmoothy': 200,  # smoothing length scale
-        'filtername': 'hanning',
-        'nmseitol': 0.75,
-        'grid_coord_check': 'LL',
-        'grid_filename': '',  # should be none if creating background Grid!  becomes best guess grid
-        'data_coord_check': 'LL',
-        'outFname': 'TestOutput.nc'}
-
-
 
 ######################################################################
 # open a new netCDF file for writing.
-######################################################################
-# save file to output
-ncfile = netcdf.netcdf_file(outFname, 'w')
-ncfile.history = 'Topo/Bathy DEM created using XXX'
-
-# create the lat and lon dimensions.
-ncfile.createDimension('y_utm', N)
-ncfile.createDimension('x_utm', M)
-
-# Define the coordinate variables. They will hold the coordinate
-# information, that is, the latitudes and longitudes.
-y_utm = ncfile.createVariable('y_utm','float32',('y_utm',))
-x_utm = ncfile.createVariable('x_utm','float32',('x_utm',))
-
-# Assign units attributes to coordinate var data. This attaches a
-# text attribute to each of the coordinate variables, containing the
-# units.
-y_utm.units = 'meters'
-x_utm.units = 'meters'
-
-# write data to coordinate vars.
-y_utm[:] = y_out
-x_utm[:] = x_out
-
-# create the pressure and temperature variables 
-z = ncfile.createVariable('z','float32',('x_utm','y_utm'))
-mse = ncfile.createVariable('mean_square_error','float32',('x_utm','y_utm'))
-nmse = ncfile.createVariable('normalized_mean_square_error','float32',('x_utm','y_utm'))
-msr = ncfile.createVariable('mean_square_residuals','float32',('x_utm','y_utm'))
-
-# set the units attribute.
-z.units =  'meters'
-mse.units =  'meters'
-nmse.units =  'meters'
-msr.units =  'meters'
-
-z.long_name =  'gridded elevation (datum NAVD88)'
-mse.long_name =  'mean square error of the gridded surface'
-nmse.long_name =  'normalized mean square error of the gridded surface'
-msr.long_name =  'mean square residual of the gridded surface'
-
-# write data to variables.
-z[:] = zi
-mse[:] = msei
-nmse[:] = nmsei
-msr[:] = msri
-
-# having trouble getting these inputs to write correctly
-# also write the inputs used
-#ncfile.createDimension('smoothing_params',3)
-#ncfile.createVariable('smoothing_params','i',('smoothing_params',))
-#smoothing_params[:] = (msmoothx, msmoothy, msmootht)
+# ######################################################################
+# # save file to output
+# ncfile = netcdf.netcdf_file(outFname, 'w')
+# ncfile.history = 'Topo/Bathy DEM created using XXX'
 #
-#ncfile.createDimension('filtername',1)
-#ncfile.createVariable('filtername','S1',('filtername',))
-#filtername = filtername
+# # create the lat and lon dimensions.
+# ncfile.createDimension('y_utm', N)
+# ncfile.createDimension('x_utm', M)
 #
-#ncfile.createDimension('nmseitol',1)
-#ncfile.createVariable('nmseitol','f4',('nmseitol',))
-#nmseitol[:] = nmseitol
-#nmseitol.long_name =  'normalized mean square error tolerance'
-
-# close the file.
-ncfile.close()
-print '*** SUCCESS writing DEM to netcdf file!'  
+# # Define the coordinate variables. They will hold the coordinate
+# # information, that is, the latitudes and longitudes.
+# y_utm = ncfile.createVariable('y_utm','float32',('y_utm',))
+# x_utm = ncfile.createVariable('x_utm','float32',('x_utm',))
+#
+# # Assign units attributes to coordinate var data. This attaches a
+# # text attribute to each of the coordinate variables, containing the
+# # units.
+# y_utm.units = 'meters'
+# x_utm.units = 'meters'
+#
+# # write data to coordinate vars.
+# y_utm[:] = y_out
+# x_utm[:] = x_out
+#
+# # create the pressure and temperature variables
+# z = ncfile.createVariable('z','float32',('x_utm','y_utm'))
+# mse = ncfile.createVariable('mean_square_error','float32',('x_utm','y_utm'))
+# nmse = ncfile.createVariable('normalized_mean_square_error','float32',('x_utm','y_utm'))
+# msr = ncfile.createVariable('mean_square_residuals','float32',('x_utm','y_utm'))
+#
+# # set the units attribute.
+# z.units =  'meters'
+# mse.units =  'meters'
+# nmse.units =  'meters'
+# msr.units =  'meters'
+#
+# z.long_name =  'gridded elevation (datum NAVD88)'
+# mse.long_name =  'mean square error of the gridded surface'
+# nmse.long_name =  'normalized mean square error of the gridded surface'
+# msr.long_name =  'mean square residual of the gridded surface'
+#
+# # write data to variables.
+# z[:] = zi
+# mse[:] = msei
+# nmse[:] = nmsei
+# msr[:] = msri
+#
+# # having trouble getting these inputs to write correctly
+# # also write the inputs used
+# #ncfile.createDimension('smoothing_params',3)
+# #ncfile.createVariable('smoothing_params','i',('smoothing_params',))
+# #smoothing_params[:] = (msmoothx, msmoothy, msmootht)
+# #
+# #ncfile.createDimension('filtername',1)
+# #ncfile.createVariable('filtername','S1',('filtername',))
+# #filtername = filtername
+# #
+# #ncfile.createDimension('nmseitol',1)
+# #ncfile.createVariable('nmseitol','f4',('nmseitol',))
+# #nmseitol[:] = nmseitol
+# #nmseitol.long_name =  'normalized mean square error tolerance'
+#
+# # close the file.
+# ncfile.close()
+# print '*** SUCCESS writing DEM to netcdf file!'
 
