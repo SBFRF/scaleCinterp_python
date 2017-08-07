75dc87dba0e66b30681574c8d21633641ccc9e24
David.L.Young <david.l.young@erdc.dren.mil>
2017-08-07 16:19:53 -0700

    tweaks to this so that it will run for spike to debug

--- scratch.py
+++ scratch.py
@@ -1,8 +1,18 @@
-from sblib import geoprocess as gp
-from scalecInterp_python.DEM_generator import DEM_generator
+from DEM_generator import DEM_generator
+import netCDF4 as nc
+import numpy as np
+import os
+
 # Call dataBuilder to construct data in necessary format for interpolation
 # filelist = list_files(datapath, datatype)  # creates a list of files to be interpolated
 filelist = ['http://134.164.129.55/thredds/dodsC/FRF/survey/gridded/FRF_20160726_1121_FRF_NAVD88_LARC_GPS_UTC_v20170320_grid_latlon.nc'] #''/home/spike/repos/scalecInterp_python/FRF_20160726_1121_FRF_NAVD88_LARC_GPS_UTC_v20170320.nc',]  # files with NEW data that are in background grid
+bathy = nc.Dataset(filelist[0])
+
+xV = bathy['xFRF'][:]
+yV = bathy['yFRF'][:]
+dataZ = bathy['elevation'][:]
+dataZ = dataZ[0, :]
+dataX, dataY = np.meshgrid(xV, yV)
 
 x0, y0 = 1600, 1600  # north east corner of grid
 x1, y1 = 0, -200     # south west corner of grid
@@ -20,12 +30,16 @@ dict = {'x0': x0,    #gp.FRFcoord(x0, y0)['Lon'],  # -75.47218285,
         'grid_coord_check': 'FRF',
         'grid_filename': '',  # should be none if creating background Grid!  becomes best guess grid
         'data_coord_check': 'FRF',
-        'filelist': filelist
+        'xFRF_s': dataX,
+        'yFRF_s': dataY,
+        'Z_s': dataZ,
         }
 
 out = DEM_generator(dict)
 
 from matplotlib import pyplot as plt
+fig_loc = 'C:\Users\dyoung8\Desktop\David Stuff\Projects\CSHORE\Bathy Interpolation\Test Figures'
+fig_name = 'test.png'
 plt.figure()
 plt.subplot(221)
 plt.title('Zi')
@@ -44,3 +58,5 @@ plt.title('MSRi')
 plt.pcolor(out['x_out'], out['y_out'], out['MSRi'])
 plt.colorbar()
 plt.tight_layout()
+plt.savefig(os.path.join(fig_loc, fig_name))
+plt.close()
